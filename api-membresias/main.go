cbffbbd30e6f64cc7dd2f8f40b4ebf37adfeddcc
jeremy-jmc <jeremy.matos@utec.edu.pe>
2024-10-02 21:04:22 -0500

    fix ports

--- api-membresias/main.go
+++ api-membresias/main.go
@@ -12,6 +12,10 @@ func main() {
 
 	router := mux.NewRouter()
 
+	router.Handle("/is_alive", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("I'm alive!"))
+	}))
 	router.HandleFunc("/membresias/{dni}", GetMembresia).Methods("GET")
 	router.HandleFunc("/membresias/{dni}", CreateMembresia).Methods("POST")
 	router.HandleFunc("/membresias/{dni}", UpdateMembresia).Methods("PUT")
@@ -23,4 +27,10 @@ func main() {
 /*
 go get github.com/gorilla/mux
 go get github.com/lib/pq
+
+go run *.go
+
+https://ramadhansalmanalfarisi8.medium.com/how-to-dockerize-your-api-with-go-postgresql-gin-docker-9a2b16548520
+https://github.com/ramadhanalfarisi/go-simple-dockerizing/blob/master/docker-compose.yml
+
 */
