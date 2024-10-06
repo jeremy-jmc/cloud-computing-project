9187c40b8f494022dc203b9a6e1eef912b81d50c
JerM1609 <jeremy.matos@utec.edu.pe>
2024-10-06 15:24:03 -0500

    other modify

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 docker compose -f /home/ubuntu/cloud-computing-project/docker-compose.yaml down || true
 docker container prune -af
 docker volume prune -af
-docker compose -d -f /home/ubuntu/cloud-computing-project/docker-compose.yaml up --build
+docker compose -f /home/ubuntu/cloud-computing-project/docker-compose.yaml up -d --build
